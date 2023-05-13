2f55b64517039d01fd3cfe138068f00a93f20825
Andrei Varzar <43003757+wizzandrew@users.noreply.github.com>
2023-05-14 01:29:41 +0200

    added science page

--- src/pages/Science.tsx
+++ src/pages/Science.tsx
@@ -1,5 +1,53 @@
 import React from "react";
+import News from "../components/NewsComponent";
+import { useAppSelector } from "../redux/hooks";
+import useFetchHook from "../shared/fetchHook";
 
 export default function Science() {
-  return <div>Science</div>;
+  // global state
+  const topheadings = useAppSelector(
+    (state) => state.topHeadings.topHeadingsCategory
+  );
+  const requestError = useAppSelector((state) => state.topHeadings.error);
+  const fetchNews = useFetchHook();
+
+  return (
+    <>
+      <div className="holder" style={{ backgroundColor: "#B80000" }}>
+        <div className="container">
+          <div className="row">
+            <div className="newsCategoryBanner col-12">
+              <h2>SCIENCE</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <button
+              onClick={() => {
+                fetchNews.fetchNewsByCategory("science");
+              }}
+            >
+              Load news
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <div className="newsCategoryArticles">
+              <News
+                page="science"
+                articles={topheadings}
+                requestError={requestError}
+                articlesCategory="Science"
+                newsByCategory={true}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
