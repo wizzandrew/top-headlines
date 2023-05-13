3200f67ab074c2b6ca2711adfbe67f5881cd18a8
Andrei Varzar <43003757+wizzandrew@users.noreply.github.com>
2023-05-14 01:22:39 +0200

    added health page

--- src/pages/Health.tsx
+++ src/pages/Health.tsx
@@ -1,5 +1,53 @@
 import React from "react";
+import News from "../components/NewsComponent";
+import { useAppSelector } from "../redux/hooks";
+import useFetchHook from "../shared/fetchHook";
 
 export default function Health() {
-  return <div>Health</div>;
+  // global state
+  const topheadings = useAppSelector(
+    (state) => state.topHeadings.topHeadingsCategory
+  );
+  const requestError = useAppSelector((state) => state.topHeadings.error);
+  const fetchNews = useFetchHook();
+
+  return (
+    <>
+      <div className="holder" style={{ backgroundColor: "#B80000" }}>
+        <div className="container">
+          <div className="row">
+            <div className="newsCategoryBanner col-12">
+              <h2>HEALTH</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <button
+              onClick={() => {
+                fetchNews.fetchNewsByCategory("health");
+              }}
+            >
+              Load news
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <div className="newsCategoryArticles">
+              <News
+                page="health"
+                articles={topheadings}
+                requestError={requestError}
+                articlesCategory="Health"
+                newsByCategory={true}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
